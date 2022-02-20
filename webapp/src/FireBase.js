a9491fd267b36ae0a962a5c76581c941fc6324a3
Deepu19g <62106634+Deepu19g@users.noreply.github.com>
2022-02-20 13:11:44 +0530

    Update FireBase.js

--- webapp/src/FireBase.js
+++ webapp/src/FireBase.js
@@ -9,7 +9,14 @@ import {
 
 // Your web app's Firebase configuration
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBgqvMkCutELhVJ1R3p1SsMpfRZCLrxXP4",
+  authDomain: "collegeapp-a6ac6.firebaseapp.com",
+  projectId: "collegeapp-a6ac6",
+  storageBucket: "collegeapp-a6ac6.appspot.com",
+  messagingSenderId: "308645680920",
+  appId: "1:308645680920:web:31cbc1315f695c25ea306d"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
